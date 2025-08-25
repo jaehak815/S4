2df1f1676a0df1852f31baa480796a8f7e4ae5ef
Jay <rright815@gmail.com>
2025-08-25 23:48:04 +0800

    88

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,5 +1,5 @@
 import { CurrencyPipe } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { InvestmentService } from '../investment.service';
 
 @Component({
@@ -12,8 +12,7 @@ import { InvestmentService } from '../investment.service';
 export class InvestmentResultsComponent {
 private investmentService = inject(InvestmentService);
 
-get results(){
-  return this.investmentService.resultData;
-}
+results = computed(()=> this.investmentService.resultData());
+// results = this.investmentService.resultData.asReadonly();
 
 }
