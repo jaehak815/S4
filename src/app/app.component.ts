222c0f342b5def5bfae1b07ad12db558c5322f80
Jay <rright815@gmail.com>
2025-08-25 22:50:39 +0800

    82

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
+import type { InvestmentInput } from './investment-input.model';
 
 @Component({
   selector: 'app-root',
@@ -9,13 +10,9 @@ import { UserInputComponent } from './user-input/user-input.component';
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  onCalculateInvestmentResults(data:
-    {initialInvestment: number,
-    duration: number,
-    expectedReturn: number,
-    annualInvestment: number}
-  ) {
-    const {initialInvestment,annualInvestment,expectedReturn,duration}= data;
+  onCalculateInvestmentResults(data: InvestmentInput) {
+    const { initialInvestment, annualInvestment, expectedReturn, duration } =
+      data;
     const annualData = [];
     let investmentValue = initialInvestment;
 
@@ -35,6 +32,6 @@ export class AppComponent {
       });
     }
 
-   console.log(annualData);
+    console.log(annualData);
   }
 }
